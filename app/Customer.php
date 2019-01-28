bd84451f4be3cd95330b55e03f022608bd1737a8
Bustillox <bustillox@gmail.com>
2019-01-27 23:21:13 -0700

    Provider model and create migration

--- app/Customer.php
+++ app/Customer.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     protected $fillable = ['name', 'document_type', 'document_number', 'address', 'phone', 'email'];
+
+    public function provider(){
+        return $this->hasOne('App\Provider');
+    }
+
 }
---
+++ database/migrations/2019_01_28_052014_create_providers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->integer('id')->unsigned();
+            $table->string('contact', 50)->nullable();
+            $table->string('contact_phone', 50)->nullable();
+            
+            $table->foreign('id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('providers');
+    }
+}
